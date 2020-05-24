4e7b1a677f33bd00df8fc085182d926b71c0f0d0
KevinCaires <kevincaires@ymail.com>
2020-05-24 18:48:24 -0300

    ADD: Função de ajuda e pylint: disable

--- aiwendil/aiwendil.py
+++ aiwendil/aiwendil.py
@@ -1,41 +1,59 @@
 import discord  # pylint: disable=import-error
 from discord.ext import commands  # pylint: disable=import-error
-from settings.common import API_URL
-from utils.tools import get_gql_client
-from utils.queries import get_job_group
+from settings.common import API_URL  # pylint: disable=import-error, no-name-in-module
+from utils.tools import get_gql_client  # pylint: disable=import-error
+from utils.queries import get_job_group  # pylint: disable=import-error
 from os import system as sys
 
 # Módulo principal do bot.
 
-client = commands.Bot(command_prefix='-')
+client = commands.Bot(command_prefix='--')
 
-@client.event()
+@client.event
 async def on_ready():
     sys('clear')
     print('<----------On---------->')
 
 
 @client.command()
-async def jobgroup(bot, _input):
+async def h(bot):
+    """
+    Informa os comandos para consultas, cadastro e criações.
+    """
+    username = '<@!' + str(bot.author.id) + '>'
+
+    body = (f'''
+        Olá {username}, em que posso ajuda-lo?
+
+        --job_group : <name> : Mostra as informações do grupo informado.
+    ''')
+
+    return await bot.send(body)
+
+@client.command()
+async def job_group(bot, _input):
     """
     Mostra o job group pelo nome.
     """
+    username = '<@!' + str(bot.author.id) + '>'
+    payload = get_job_group(_input)
     api_client = get_gql_client(API_URL)
-    response = api_client.execute(_input)
-    user = bot.author.id
+    response = api_client.execute(payload)
 
     if not _input:
         return await bot.send(f'É necessário o nome grupo de atividades!')
     
     else:
         edges = response['jobGroup']['edges']
+        print(f'Edges ========> {edges}')
         node = edges[0].get('node') if edges else None
+        print(f'Node =========> {node}')
         job_name = node.get('name') if node else None
         job_description = node.get('description') if node else None
 
         embed = discord.Embed(color=0x1B1E94, type='rich')
         embed.add_field(name=f'{job_name}', value=f'{job_description}', inline=False)
 
-        description = f'{user}, aqui está:'
+        description = f'{username}, aqui está:'
 
         return await bot.send(description, embed=embed)
